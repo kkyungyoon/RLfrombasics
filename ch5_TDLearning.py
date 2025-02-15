e40ab743623697c6e63cfc1490e86d0e67355666
kkyungyoon <ruddbs803@gmail.com>
2025-02-15 17:44:12 +0900

    study ch5_TDLearning.py

--- ch5_TDLearning.py
+++ ch5_TDLearning.py
@@ -1,3 +1,12 @@
+"""
+MDP : unknown = model-free
+prediction 방법 : MC, TD
+그 중 TD prediction
+- MC와 environment, agent는 동일
+- main함수 : 
+    1) 에피소드가 끝나고 업데이트하는게 아니라, 한번의 액션마다 테이블이 업데이트됨
+    2) 업데이트 폭인 alpha가 MC보다 크다.(왜냐, TD가 MC에 비해 학습의 변동성이 작은 덕분에 그만큼 큰 폭의 업데이트 가능)
+"""
 import random
 import numpy as np
 
@@ -81,14 +90,20 @@ def main():
     reward = -1
     alpha = 0.01
 
+    # 50000 에피소드
     for k in range(50000):
         done = False
+        # 에피소드가 끝날 때까지 반복
         while not done:
+            # 현재 state 저장
             x, y = env.get_state()
             action = agent.select_action()
             (x_prime, y_prime), reward, done = env.step(action)
+            # 업데이트된 state 저장
             x_prime, y_prime = env.get_state()
+            # TD(0) 업데이트 식 : V(s) ← V(s) + α * (R + γ * V(s') - V(s))
             data[x][y] = data[x][y] + alpha*(reward+gamma*data[x_prime][y_prime]-data[x][y])
+        # 에피소드 종료 후 환경 초기화
         env.reset()
             
     for row in data:
